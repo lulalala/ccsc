e9b8ba02298bc28658a266d60b7273027880db52
lulalala <mark@goodlife.tw>
2016-01-23 23:40:07 +0800

    link post to fountain if fountain_id exists

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   serialize :misc, Hash
+
+  attr_accessor :fountain_id
+
+  after_save :link_to_fountain
+
+  def link_to_fountain
+    if fountain_id
+      FountainEntry.find_or_create_by(post_id:id, fountain_id:fountain_id)
+    end
+  end
 end
