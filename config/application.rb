9266ab5e4b93a37cdc0c84bf71eab64c2095e68b
lulalala <mark@goodlife.tw>
2016-02-27 21:18:07 +0800

    Stop scaffold from generating specs/helpers/js/css

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module Ccsc
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
