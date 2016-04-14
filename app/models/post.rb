f6a11ee92d34ca3d425d62b35a47de2f4a74b0c0
ChinLan <nilsoul@hotmail.com.tw>
2016-04-14 14:12:58 +0800

    文章加上標籤功能

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,22 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :owner, polymorphic: true
+  acts_as_taggable
 
   serialize :misc, Hash
 
   attr_accessor :fountain_id
 
   after_save :link_to_fountain
+  before_save :set_special_tags
 
   def link_to_fountain
     if fountain_id
       FountainEntry.find_or_create_by(post_id:id, fountain_id:fountain_id)
     end
+  end
+
+  def set_special_tags
+    set_tag_list_on("#{owner.class.name.underscore}_tags", tag_list)
   end
 end
