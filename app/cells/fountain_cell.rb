c8a079bb80d8b389b27a5b9b65cadb2aeb463734
lulalala <mark@goodlife.tw>
2016-05-29 13:58:21 +0800

    table

---
+++ app/cells/fountain_cell.rb
@@ -0,0 +1,35 @@
+class FountainCell < Cell::Rails
+  class CategorySection
+    attr_accessor :entries
+    attr_accessor :category
+
+    def initialize(category)
+      self.category = category
+      self.entries = []
+    end
+
+    def add_entry(entry)
+      entries << entry
+    end
+  end
+
+  def catalogue(fountain)
+    @fountain = fountain
+
+    @sections = []
+
+    previous_category = :not_set
+    @fountain.fountain_entries.order(order: :asc).each do |entry|
+      if entry.category == previous_category
+        @sections.last.add_entry(entry)
+      else
+        section = CategorySection.new(entry.category)
+        section.add_entry(entry)
+        @sections << section
+        previous_category = entry.category
+      end
+    end
+
+    render
+  end
+end
