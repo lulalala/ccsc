920b0b254d241e2ce5712187bd7f8d2bf861f580
lulalala <mark@goodlife.tw>
2016-06-11 21:48:34 +0800

    Show banner at fountains#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def banner_tag(name)
+    if banner = Banner.find_by(name: name)
+      image_tag banner.image.url, class:'img-responsive'
+    else
+      nil
+    end
+  end
 end
