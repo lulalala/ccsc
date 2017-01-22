2d89f0013f4bc5096b05c90c3e5013adc4f09e85
lulalala <mark@goodlife.tw>
2017-01-22 20:27:48 +0800

    Remove unused code

--- app/controllers/comment/posts_controller.rb
+++ app/controllers/comment/posts_controller.rb
@@ -1,10 +1,7 @@
 module Comment
   class PostsController < ApplicationController
-    before_action :set_post, only: [:destroy]
-
     # For creating new topic or replying
     def create
-
       outcome = Comment::Create.run(
         owner: Comment::Post.find_owner(params[:owner_type], params[:owner_id]),
         author: params[:author],
@@ -22,13 +19,6 @@ module Comment
           format.html { redirect_to :back }
         end
       end
-    end
-
-  private
-
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
     end
   end
 end
