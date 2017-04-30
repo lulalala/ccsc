69339201821b5066160a9aa2e3ebc9bb703d7cdf
lulalala <mark@goodlife.tw>
2017-04-30 19:42:55 +0800

    fix migration

--- db/migrate/20170327155812_add_commented_at_to_comment_topic.rb
+++ db/migrate/20170327155812_add_commented_at_to_comment_topic.rb
@@ -1,9 +1,11 @@
 class AddCommentedAtToCommentTopic < ActiveRecord::Migration
   def change
     add_column :comment_topics, :commented_at, :datetime
-    Comment::Topic.all.each do |ct|
-      ct.commented_at = ct.posts.last.created_at
-      ct.save
+    Comment::Topic.find_each do |ct|
+      if p = ct.posts.last
+        ct.commented_at = p.created_at
+        ct.save
+      end
     end
   end
 end
