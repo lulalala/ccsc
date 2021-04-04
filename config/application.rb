5b3524f240157293f9e30a344c46cd3b33d87d92
lulalala <mark@goodlife.tw>
2021-04-04 18:37:44 +0800

    Upgrade to 5.2

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,18 @@
 require_relative 'app_config'
+require_relative 'boot'
 
-require File.expand_path('../boot', __FILE__)
-
-# Based on https://github.com/rails/rails/blob/v6.0.1/railties/lib/rails/all.rb
-# Only load the railties we need instead of loading everything
-require 'rails'
-
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_view/railtie'
-require 'action_mailer/railtie'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,7 +24,8 @@ module Ccsc
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,205 +10,205 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210404025003) do
+ActiveRecord::Schema.define(version: 2021_04_04_102626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "banners", force: :cascade do |t|
-    t.string   "name"
-    t.string   "image"
+  create_table "banners", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "parent_id"
-    t.string   "scope"
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "parent_id"
+    t.string "scope"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["parent_id"], name: "index_categories_on_parent_id", using: :btree
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
 
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    limit: 30
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
+  create_table "ckeditor_assets", id: :serial, force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.integer "assetable_id"
+    t.string "assetable_type", limit: 30
+    t.string "type", limit: 30
+    t.integer "width"
+    t.integer "height"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
-    t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+    t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+    t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
   end
 
-  create_table "comment_posts", force: :cascade do |t|
-    t.integer  "topic_id"
-    t.string   "author"
-    t.text     "content"
-    t.text     "content_html"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "comment_posts", id: :serial, force: :cascade do |t|
+    t.integer "topic_id"
+    t.string "author"
+    t.text "content"
+    t.text "content_html"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "comment_topics", force: :cascade do |t|
-    t.integer  "owner_id"
-    t.string   "owner_type"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "comment_topics", id: :serial, force: :cascade do |t|
+    t.integer "owner_id"
+    t.string "owner_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "commented_at"
-    t.index ["owner_type", "owner_id"], name: "index_comment_topics_on_owner_type_and_owner_id", using: :btree
+    t.index ["owner_type", "owner_id"], name: "index_comment_topics_on_owner_type_and_owner_id"
   end
 
-  create_table "culture_entries", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "category_id"
-    t.index ["category_id"], name: "index_culture_entries_on_category_id", using: :btree
+  create_table "culture_entries", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_culture_entries_on_category_id"
   end
 
-  create_table "forum_boards", force: :cascade do |t|
-    t.string   "name"
-    t.text     "config"
-    t.integer  "order"
+  create_table "forum_boards", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "config"
+    t.integer "order"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "forum_topics", force: :cascade do |t|
-    t.string   "title"
-    t.boolean  "sticky"
-    t.integer  "board_id"
+  create_table "forum_topics", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.boolean "sticky"
+    t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.integer  "position"
-    t.index ["board_id"], name: "index_forum_topics_on_board_id", using: :btree
+    t.integer "user_id"
+    t.integer "position"
+    t.index ["board_id"], name: "index_forum_topics_on_board_id"
   end
 
-  create_table "groups", force: :cascade, comment: "內部團體" do |t|
-    t.string   "name",                    comment: "名稱"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "infos", force: :cascade do |t|
-    t.string   "seo_name"
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["seo_name"], name: "index_infos_on_seo_name", using: :btree
-  end
-
-  create_table "notices", force: :cascade, comment: "新聞訊息" do |t|
-    t.integer  "group_id"
-    t.string   "title",                   comment: "標題"
-    t.text     "body",                    comment: "內文"
+  create_table "groups", id: :serial, comment: "內部團體", comment: "內部團體", force: :cascade do |t|
+    t.string "name", comment: "名稱"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "periodical_entries", force: :cascade do |t|
-    t.integer  "periodical_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "order"
-    t.string   "category"
-    t.index ["periodical_id"], name: "index_periodical_entries_on_periodical_id", using: :btree
-  end
-
-  create_table "periodicals", force: :cascade do |t|
-    t.integer  "issue"
-    t.string   "title"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.text     "catalogue"
-    t.string   "image"
-    t.string   "type"
-    t.boolean  "public",     default: false, null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.string   "author"
-    t.integer  "user_id"
-    t.text     "misc"
+  create_table "infos", id: :serial, force: :cascade do |t|
+    t.string "seo_name"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "owner_id"
-    t.string   "owner_type"
-    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+    t.index ["seo_name"], name: "index_infos_on_seo_name"
   end
 
-  create_table "schedules", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "group_id"
+  create_table "notices", id: :serial, comment: "新聞訊息", comment: "新聞訊息", force: :cascade do |t|
+    t.integer "group_id"
+    t.string "title", comment: "標題"
+    t.text "body", comment: "內文"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "taggings", force: :cascade do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context",       limit: 128
+  create_table "periodical_entries", id: :serial, force: :cascade do |t|
+    t.integer "periodical_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "order"
+    t.string "category"
+    t.index ["periodical_id"], name: "index_periodical_entries_on_periodical_id"
+  end
+
+  create_table "periodicals", id: :serial, force: :cascade do |t|
+    t.integer "issue"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "catalogue"
+    t.string "image"
+    t.string "type"
+    t.boolean "public", default: false, null: false
+  end
+
+  create_table "posts", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "author"
+    t.integer "user_id"
+    t.text "misc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "owner_id"
+    t.string "owner_type"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "schedules", id: :serial, force: :cascade do |t|
+    t.text "body"
+    t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "taggable_id"
+    t.string "taggable_type"
+    t.integer "tagger_id"
+    t.string "tagger_type"
+    t.string "context", limit: 128
     t.datetime "created_at"
-    t.index ["context"], name: "index_taggings_on_context", using: :btree
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
-    t.index ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
-    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
-    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy", using: :btree
-    t.index ["taggable_id"], name: "index_taggings_on_taggable_id", using: :btree
-    t.index ["taggable_type"], name: "index_taggings_on_taggable_type", using: :btree
-    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type", using: :btree
-    t.index ["tagger_id"], name: "index_taggings_on_tagger_id", using: :btree
+    t.index ["context"], name: "index_taggings_on_context"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string  "name"
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true, using: :btree
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.string   "invitation_token"
+    t.string "unconfirmed_email"
+    t.string "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
-    t.integer  "invitation_limit"
-    t.integer  "invited_by_id"
-    t.string   "invited_by_type"
-    t.integer  "invitations_count",      default: 0
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
-    t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
-    t.index ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.integer "invitation_limit"
+    t.integer "invited_by_id"
+    t.string "invited_by_type"
+    t.integer "invitations_count", default: 0
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+    t.index ["invitations_count"], name: "index_users_on_invitations_count"
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comment_posts", "comment_topics", column: "topic_id"
