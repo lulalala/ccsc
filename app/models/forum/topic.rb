fd11e91bdd9d1ea93f22be6ef08b56caefb083f4
lulalala <mark@goodlife.tw>
2021-04-06 20:59:40 +0800

    Enforce title presence

--- app/models/forum/topic.rb
+++ app/models/forum/topic.rb
@@ -3,5 +3,7 @@ module Forum
     belongs_to :board, class_name: "Forum::Board", optional: true
     has_one :comment_topic, class_name:'Comment::Topic', as: :owner
     acts_as_list scope: :board
+
+    validates :title, presence: true
   end
 end
