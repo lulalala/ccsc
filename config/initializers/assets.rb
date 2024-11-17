cbb8dc248dd1af413b5eebdfb1c991ae46d9f785
lulalala <mark@goodlife.tw>
2024-11-17 13:55:08 +0800

    Upgrade to rail 7

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,9 +9,11 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( ckeditor/* )
 
 Rails.configuration.assets.paths.reject! do |path|
   path.to_s.include?('rails-assets-bootstrap')
 end
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
